c86a9d1c6c10cf298851758bb8e0bef19889e9cf
Tiurin Artur <tyurinart14@gmail.com>
2022-07-23 00:26:57 +0300

    completed the task

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,54 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpRequest, HttpResponse
+import random
+
+politic = {
+    1: {"name": "prime minister", "fullname": "Boris Johnson", "id": 1},
+    2: {"name": "european commission", "fullname": "Ursula von der Leyen", "id": 2},
+    3: {"name": "potato", "fullname": "Alexander Lukashenko", "id": 3},
+    4: {"name": "president", "fullname": "Volodimir Zelenskiy", "id": 4}
+}
+
+
+def first_page(request):
+    return HttpResponse('Tiurin Home Work')
+
+
+def page_view(request: HttpRequest, article_id, article_slug):
+    return HttpResponse(f"{article_id} {article_slug}")
+
+
+def politic_list(request):
+    return HttpResponse(politic.values())
+
+
+def pass_word(request, password):
+    good_password = "".join(x for x in password if x.isalnum())
+    if len(password) == 8:
+        if good_password == password:
+            return HttpResponse("Good password")
+        else:
+            return HttpResponse("No, no, no, no this password is bad")
+    else:
+        return HttpResponse("No, no, no, no this password is bad")
+
+
+def random_pass(request, length):
+    password = ''
+    for x in range(int(length)):
+        password += random.choice(list("1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ"))
+    return HttpResponse(f"OK, take your password:  {password}")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', first_page),
+    path('home', first_page),
+    path('homepage', first_page),
+    path('article/<int:article_id>/<str:article_slug>', page_view),
+    path('politic', politic_list),
+    path('password/<password>', pass_word),
+    path('password/generate/<length>', random_pass)
 ]
